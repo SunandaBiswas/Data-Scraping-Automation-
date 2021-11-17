76291f75ec619ae5ac7ea10959e1a41452c6ea95
Sunanda Biswas <81425092+SunandaBiswas@users.noreply.github.com>
2021-11-17 10:07:55 +0600

    Update yfinance.py

--- yfinance.py
+++ yfinance.py
@@ -1,5 +1,5 @@
 #Download historical finance data from Yahoo!finance
-
+#read more: https://pypi.org/project/yfinance/
 
 import yfinance as yf
 df = yf.download("GOOGL" , start = "1995-01-01" , interval = '1d')
